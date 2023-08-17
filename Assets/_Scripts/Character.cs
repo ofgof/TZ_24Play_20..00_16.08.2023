6ee63ff912480b059e1e4317199a616fe162a058
Voland <vovav3539@gmail.com>
2023-08-17 23:52:57 +0300

    add : added ragdall

--- Assets/_Scripts/Character.cs
+++ Assets/_Scripts/Character.cs
@@ -4,9 +4,37 @@ using UnityEngine;
 
 public class Character : MonoBehaviour, IKillable
 {
+    [SerializeField] private Animator _animator;
+    private void Start()
+    {
+        SetRigidbody(true);
+        SetCollider(false);
+    }
     public void Kill()
     {
         GameManager.OnGameEnd?.Invoke();
         Debug.Log($"[{name}] Kill");
+        SetRigidbody(false);
+        SetCollider(true);
+        _animator.enabled = false;
+    }
+
+    private void SetRigidbody(bool state)
+    {
+        Rigidbody[] rigidbodies = GetComponentsInChildren<Rigidbody>();
+        foreach (var rigidbody in rigidbodies)
+        {
+            rigidbody.isKinematic = state;
+        }
+        GetComponent<Rigidbody>().isKinematic = !state;
+    }
+    private void SetCollider(bool state)
+    {
+        Collider[] colliders = GetComponentsInChildren<Collider>();
+        foreach (var collider in colliders)
+        {
+            collider.enabled = state;
+        }
+        GetComponent<Collider>().enabled = !state;
     }
 }
